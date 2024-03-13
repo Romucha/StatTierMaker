82f56c35a6ee58ad87e1a8d2f311db7e1c7f0f9a
roman-krysyaev <roman-krysyaev@yandex.ru>
2024-03-13 23:50:34 +0300

    Added basic implementations for template factories.

---
+++ StatTierMaker.API/TierTemplateFactories/Lists/TierListTemplateFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using StatTierMaker.API.TierTemplates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatTierMaker.API.TierTemplateFactories.Lists
+{
+    public class TierListTemplateFactory : ITierListTemplateFactory
+    {
+        private readonly ILogger<TierListTemplateFactory> logger;
+
+        public TierListTemplateFactory(ILogger<TierListTemplateFactory> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task<TierListTemplate> CreateAsync(string name, string description, TierEntityTemplate tierEntityTemplate)
+        {
+            try
+            {
+                logger.LogInformation($"Creating {nameof(TierListTemplate)} with name: {name}; description: {description}");
+                return await Task.FromResult(new TierListTemplate()
+                {
+                    Name = name,
+                    Description = description,
+                    TierEntityTemplate = tierEntityTemplate
+                });
+            }
+            catch (Exception ex) 
+            {
+                logger.LogError(ex, nameof(TierListTemplateFactory));
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Creation done.");
+            }
+        }
+    }
+}
---
+++ StatTierMaker.API/TierTemplateFactories/Parameters/TierParameterTemplateFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using StatTierMaker.API.TierTemplates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatTierMaker.API.TierTemplateFactories.Parameters
+{
+    public class TierParameterTemplateFactory : ITierParameterTemplateFactory
+    {
+        private readonly ILogger<TierParameterTemplateFactory> logger;
+
+        public TierParameterTemplateFactory(ILogger<TierParameterTemplateFactory> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task<TierParameterTemplate> CreateAsync(string name, string description, double coefficient)
+        {
+            try
+            {
+                logger.LogInformation($"Creating {nameof(TierParameterTemplate)} with name: {name}; description: {description}; coefficient: {coefficient}");
+                return await Task.FromResult(new TierParameterTemplate()
+                {
+                    Name = name,
+                    Description = description,
+                    Coefficient = coefficient
+                });
+            }
+            catch (Exception ex) 
+            {
+                logger.LogError(ex, this.GetType().Name);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Creation done.");
+            }
+        }
+    }
+}
