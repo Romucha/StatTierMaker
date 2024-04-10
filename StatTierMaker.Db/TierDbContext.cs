ad80333e8e5277a686bbb629d190985450d715a1
roman-krysyaev <roman-krysyaev@yandex.ru>
2024-04-10 22:53:21 +0300

    Added base class for database context.

---
+++ StatTierMaker.Db/TierDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using StatTierMaker.API.Tiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatTierMaker.Db
+{
+    public class TierDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public DbSet<TierList> TierLists { get; set; }
+
+        public TierDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
