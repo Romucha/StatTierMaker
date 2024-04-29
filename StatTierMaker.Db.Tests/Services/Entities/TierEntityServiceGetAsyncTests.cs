815394ff34eeb54770a2ae3b5ac7216659e7093c
roman-krysyaev <krysyaev_ry@ascon.ru>
2024-04-29 04:34:07 +0300

    Added tests for getting entities in tier entity service.

--- StatTierMaker.Db.Tests/Services/Entities/TierEntityServiceGetAsyncTests.cs
+++ StatTierMaker.Db.Tests/Services/Entities/TierEntityServiceGetAsyncTests.cs
@@ -1,7 +1,12 @@
-﻿using StatTierMaker.Db.Repositories;
+﻿using StatTierMaker.Db.DTO.Requests.Entities;
+using StatTierMaker.Db.Repositories;
 using StatTierMaker.Db.Services;
+using StatTierMaker.Tests.Common.TestAPIData.Entities;
+using StatTierMaker.Tests.Common.TestDbData.Entities.Add;
+using StatTierMaker.Tests.Common.TestDbData.Entities.Get;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,21 +18,64 @@ namespace StatTierMaker.Db.Tests.Services.Entities
         [Fact]
         public async Task GetAsync_Normal()
         {
+            //arrange
+            var entity = SingularEntities.Normal();
+            await TierDbContext.TierEntities.AddAsync(entity);
+            await TierDbContext.SaveChangesAsync();
+
+            var id = TierDbContext.TierEntities.Entry(entity).Entity.Id;
+            var request = SingularGetTierEntityRequests.Normal(id);
+            //act
+            var response = await tierEntityService.GetTierEntity(request);
+            //assert
+            Assert.NotNull(response);
+            Assert.Equal(id, response.Id);
+            Assert.Equal(entity.Name, response.Name);
+            Assert.Equal(entity.Description, response.Description);
+            Assert.Equal(entity.TierId, response.TierId);
         }
 
         [Fact]
-        public async Task GetAsync_ThrowsException_WhenRequestIsInvalid()
+        public async Task GetAsync_ReturnsNull_WhenRequestIsInvalid()
         {
+            //arrange
+            var entity = SingularEntities.Normal();
+            await TierDbContext.TierEntities.AddAsync(entity);
+            await TierDbContext.SaveChangesAsync();
+
+            var request = SingularGetTierEntityRequests.Invalid();
+            //act
+            var response = await tierEntityService.GetTierEntity(request);
+            //assert
+            Assert.Null(response);
         }
 
         [Fact]
         public async Task GetAsync_ThrowsException_WhenRequestIsDefault()
         {
+            //arrange
+            var entity = SingularEntities.Normal();
+            await TierDbContext.TierEntities.AddAsync(entity);
+            await TierDbContext.SaveChangesAsync();
+
+            var request = SingularGetTierEntityRequests.Default();
+            //act
+            var response = await tierEntityService.GetTierEntity(request);
+            //assert
+            Assert.Null(response);
         }
 
         [Fact]
         public async Task GetAsync_ThrowsException_WhenRequestIsNull()
         {
+            //arrange
+            var entity = SingularEntities.Normal();
+            await TierDbContext.TierEntities.AddAsync(entity);
+            await TierDbContext.SaveChangesAsync();
+
+            var request = SingularGetTierEntityRequests.Null();
+            //act & assert
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await tierEntityService.GetTierEntity(request));
         }
     }
 }
