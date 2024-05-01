a1c4ed529151f243a22514cd5a9bbce3c74ffb7b
roman-krysyaev <krysyaev_ry@ascon.ru>
2024-05-01 03:27:57 +0300

    Added implementation for tier list service methods.

--- StatTierMaker.Db/Services/TierListService.cs
+++ StatTierMaker.Db/Services/TierListService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Logging;
+using StatTierMaker.API.Tiers;
 using StatTierMaker.Db.DTO.Requests.Lists;
+using StatTierMaker.Db.DTO.Responses.Entities;
 using StatTierMaker.Db.DTO.Responses.Lists;
 using StatTierMaker.Db.Repositories;
 using System;
@@ -20,29 +22,110 @@ namespace StatTierMaker.Db.Services
             this.logger = logger;
         }
 
-        public async Task<GetTierListsResponse> GetTierLists(GetTierListsRequest getTierListsRequest, CancellationToken cancellationToken = default)
+        public async Task<GetTierListsResponses> GetTierLists(GetTierListsRequest getTierListsRequest, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                logger.LogInformation("Getting tier lists...");
+                var lists = await UnitOfWork.TierListRepository.GetAllAsync(cancellationToken);
+                return new GetTierListsResponses
+                {
+                    TierLists = lists.Select(Mapper.Map<GetTierListsResponse>)
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Getting done.");
+            }
         }
 
         public async Task<GetTierListResponse> GetTierList(GetTierListRequest getTierListRequest, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                logger.LogInformation("Getting tier list...");
+                var list = await UnitOfWork.TierListRepository.GetAsync(getTierListRequest.Id, cancellationToken);
+                return Mapper.Map<GetTierListResponse>(list);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Getting done.");
+            }
         }
 
         public async Task<AddTierListResponse> AddTierList(AddTierListRequest addTierListRequest, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                logger.LogInformation("Adding tier list...");
+                var list = Mapper.Map<TierList>(addTierListRequest);
+                await UnitOfWork.TierListRepository.AddAsync(list, cancellationToken);
+                return new AddTierListResponse()
+                {
+                    Id = list.Id,
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Adding done.");
+            }
         }
 
         public async Task<DeleteTierListResponse> DeleteTierList(DeleteTierListRequest deleteTierListRequest, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                logger.LogInformation("Deleting tier list...");
+                await UnitOfWork.TierListRepository.DeleteAsync(deleteTierListRequest.Id, cancellationToken);
+                return new DeleteTierListResponse
+                {
+                    Id= deleteTierListRequest.Id,
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Deleting done.");
+            }
         }
 
         public async Task<UpdateTierListResponse> UpdateTierList(UpdateTierListRequest updateTierListRequest, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                logger.LogInformation("Updating tier list...");
+                var list = Mapper.Map<TierList>(updateTierListRequest);
+                await UnitOfWork.TierListRepository.UpdateAsync(list, cancellationToken);
+                return Mapper.Map<UpdateTierListResponse>(list);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Updating done.");
+            }
         }
     }
 }
